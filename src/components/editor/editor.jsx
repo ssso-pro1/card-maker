347d6a54337e40cf19756ae1e577eb577f45e701
Soyeon Jang <ssso.pro1@gmail.com>
2021-09-14 16:55:18 +0900

    ADD : card component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Card from '../card/card';
+import CardEditForm from '../card_edit/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = props => (
+const Editor = ({ cards }) => (
   <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
+    {cards.map(card => (
+      //   <Card card={card} />
+      <CardEditForm card={card} />
+    ))}
   </section>
 );
 
